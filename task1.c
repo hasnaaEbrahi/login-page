a1fe03dd08265a0757b48c64342c41d85ed2580b
hasnaaebrahim <117865720+hasnaaEbrahi@users.noreply.github.com>
2023-10-13 18:36:00 +0300

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+
+
+    char firstName[6];
+    char lastName[7];
+    int age;
+    int phone;
+printf(" Enter you firstName \n");
+gets(firstName);
+puts("Enter your lastName \n");
+gets(lastName);
+printf(" Enter your age \n");
+scanf(" =  %d\n" , &age);
+puts(" Enter your phone Num \n");
+scanf(" = %d ", &phone);
+printf ("LOGIN IS SUCCESS");
+
+
+
+
+
+    return 0;
+}
